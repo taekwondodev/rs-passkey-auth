c57e1158b73188fd97eff19c631fbd6c186c2ddd
taekwondodev <taekwondodev@outlook.it>
2025-08-24 13:06:27 +0200

    added tracing

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ use rs_passkey_auth::{
     auth::handler,
     config::{postgres::DbConfig, webauthn::WebAuthnConfig},
 };
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
     let db_config = DbConfig::from_env()?;
     let db_pool = db_config.create_pool()?;
     let _conn = db_pool.get().await.map_err(AppError::from)?;
@@ -21,7 +26,8 @@ async fn main() -> Result<(), AppError> {
         .route("/auth/register/finish", post(handler::finish_register))
         .route("auth/login/begin", post(handler::begin_login))
         .route("auth/login/finish", post(handler::finish_login))
-        .with_state(state);
+        .with_state(state)
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
     println!("Server listening on http://0.0.0.0:8080");
